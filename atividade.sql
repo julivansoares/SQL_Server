11d6c7212af227e1e50cd43cb2c7b148dd31b2a7
julivansoares <julivan.soares@outlook.com>
2023-05-24 21:57:01 -0300

    atividade_01

--- atividade.sql
+++ atividade.sql
@@ -1,1 +1,48 @@
 create database SUCOS_VENDAS_01;
+
+
+
+
+
+create database SUCOS_VENDAS_02
+
+OR (NAME -'SUCOS_VENDAS.DAT', = NOME INTERNO DO BANCO
+
+FILENAME - '/media/soares/rock/Projetos_2023_1/sql_server', = LOCAL A ONDE QUERO SALVAR   MEU  BANCO
+
+SIZE - 10MB, = TANHO INICIAL DO BANCO
+ 
+MAXSIZE -50MB, = TAMANHO MÁXIMO DO BANCO
+
+FILEGROWTH =5MB) = TAXA DE CRECIMENTO DO BANCO EX= QUANDO CHEGA A O TAMANHO MÁXIMO ELE ADD MIAS 5MB  
+ 
+ LOG ON
+ (NAME -'SUCOS_VENDAS.DAT',
+ FILENAME - '/media/soares/rock/Projetos_2023_1/sql_server',
+ SIZE - 10MB,
+ MAXSIZE -50MB,
+ FILEGROWTH =5MB
+ 
+ 
+ ) 
+
+//////////////////////////////////////////////////////
+                 TIPOS DE DADOS
+
+
+BIGINT =byte 8 
+
+INT    =byte 4 
+
+SMALLINT=byte 2 
+
+TINYINT =byte 1
+
+//////////////////////////////////////////////////////////////////////////
+               NUMEROS EXATOS
+
+
+MONEY   =byte 8
+
+SMALLMONEY=byte 4
+ 
